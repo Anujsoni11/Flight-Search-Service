8aaacd64e3d26907f45a044635ac052c91ed4bf9
Anujsoni11 <anujsonisd2@gmail.com>
2026-09-16 17:11:48 +0530

    Added city controller

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -16,6 +16,7 @@ class CityRepository{
             const city = await City.destroy({
                 where: {id: cityID}
             });
+            return true;
         }
         catch(error) {
             console.log("something went wrong in repository layer");
