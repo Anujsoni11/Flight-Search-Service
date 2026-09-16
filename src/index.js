5379c7c9d4b7f7d4bd23c6fa99b39126fd23c3df
Anujsoni11 <anujsonisd2@gmail.com>
2026-09-16 17:18:07 +0530

    Added routers to complete create Api

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
-const CityRepository = require('./repository/city-repository');
+const ApiRoutes = require('./routes/index');
 
 const setupAndStartServer = async () => {
     //create the express object
-const app = express();
+    const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(PORT, () => {
-    console.log(`server started on PORT ${PORT}`);
-    const repo = new CityRepository();
-    repo.createCity({ name:"Jabalpur" });
-});
-};
+    app.use('/api', ApiRoutes);
+
+    app.listen(PORT, () => {
+        console.log(`server started on PORT ${PORT}`);
+    });
+}
