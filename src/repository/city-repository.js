dbddb8ccc5f5a249d0f55244f0b01dc4ca36ab92
Anujsoni11 <anujsonisd2@gmail.com>
2026-09-16 17:29:43 +0530

    Added Filter to getAll cities

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,4 +1,5 @@
-const { City } = require('../models/index')
+const { City } = require('../models/index');
+const { Op }=require('sequelize');
 
 class CityRepository {
     async createCity({ name }) {
@@ -57,10 +58,21 @@ class CityRepository {
         }
     }
 
-    async getAllCities() {
-        try {
-            const cities = await City.findAll();
+    async getAllCities(filter) {  //filter can be empty also
+        try { 
+            if(filter.name){
+            const cities=await City.findAll({
+                where:{
+                    name :{
+                        [Op.startsWith]:filter.name
+                    }
+                }
+            });
             return cities;
+           }
+
+           const cities=await City.findAll();
+           return cities;
         }
         catch (error) {
             console.log("something went wrong in repository layer");
